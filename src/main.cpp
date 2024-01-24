079b609c650bc3bd20dd752bdca27bf509c2d484
Dane Petersen <thegreatsunra@gmail.com>
2024-01-24 08:29:41 -0600

    Render the background image

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "bn_core.h"
 #include "bn_regular_bg_items_background.h"
+#include "bn_regular_bg_ptr.h"
 
 int main()
 {
     bn::core::init();
+    // (520, -48) are magic numbers to align the 1280x256 .bmp image with the lower-left corner of the display
+    bn::regular_bg_ptr regular_bg = bn::regular_bg_items::background.create_bg(520, -48);
 
     while(true)
     {
