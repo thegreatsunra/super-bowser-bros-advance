16e1314ebe9dbfdb4d3a5631d4693ae0c6e8fdd7
Dane Petersen <thegreatsunra@gmail.com>
2024-01-24 11:18:33 -0600

    Move everything into a function with a loop inside it

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,46 @@
 #include "bn_sprite_items_bowser.h"
 #include "bn_sprite_items_mario.h"
 
+namespace
+{
+    void default_scene()
+    {
+        // (520, -48) are magic numbers to align the 1280x256 .bmp image with the lower-left corner of the display
+        bn::regular_bg_ptr regular_bg = bn::regular_bg_items::background.create_bg(520, -48);
+
+        // (0, 40) is a magic number to make the sprite appear on the ground
+        bn::sprite_ptr mario = bn::sprite_items::mario.create_sprite(0, 40);
+        // (32, 32) is a magic number to make the sprite appear on the ground
+        bn::sprite_ptr bowser = bn::sprite_items::bowser.create_sprite(32, 32);
+        while(true)
+            {
+            if(bn::keypad::left_held())
+            {
+                bowser.set_x(bowser.x() - 1);
+            }
+            else if(bn::keypad::right_held())
+            {
+                bowser.set_x(bowser.x() + 1);
+            }
+            if(bn::keypad::up_held())
+            {
+                bowser.set_y(bowser.y() - 1);
+            }
+            else if(bn::keypad::down_held())
+            {
+                bowser.set_y(bowser.y() + 1);
+            }
+            bn::core::update();
+        }
+    }
+}
+
 int main()
 {
     bn::core::init();
-    // (520, -48) are magic numbers to align the 1280x256 .bmp image with the lower-left corner of the display
-    bn::regular_bg_ptr regular_bg = bn::regular_bg_items::background.create_bg(520, -48);
-
-    // (0, 40) is a magic number to make the sprite appear on the ground
-    bn::sprite_ptr mario = bn::sprite_items::mario.create_sprite(0, 40);
-    // (32, 32) is a magic number to make the sprite appear on the ground
-    bn::sprite_ptr bowser = bn::sprite_items::bowser.create_sprite(32, 32);
 
     while(true)
     {
-        if(bn::keypad::left_held())
-        {
-            bowser.set_x(bowser.x() - 1);
-        }
-        else if(bn::keypad::right_held())
-        {
-            bowser.set_x(bowser.x() + 1);
-        }
-        if(bn::keypad::up_held())
-        {
-            bowser.set_y(bowser.y() - 1);
-        }
-        else if(bn::keypad::down_held())
-        {
-            bowser.set_y(bowser.y() + 1);
-        }
-        bn::core::update();
+        default_scene();
     }
 }
