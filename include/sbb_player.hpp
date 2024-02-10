54ca57b1b867ec683f4cb6206b3a8f469410e57b
Dane Petersen <thegreatsunra@gmail.com>
2024-02-10 12:13:24 -0600

    Keep implementing things

--- include/sbb_player.hpp
+++ include/sbb_player.hpp
@@ -22,11 +22,15 @@ namespace sbb
             bn::optional<bn::camera_ptr> m_camera;
             bn::optional<bn::span<const bn::affine_bg_map_cell>> m_map_cells;
             bn::optional<bn::affine_bg_ptr> m_map;
+            void m_update_camera(int lerp);
         public:
             Player(bn::sprite_ptr m_sprite);
 
+            void t_delete_data();
             void t_hide();
+            void t_reset();
             void t_spawn(bn::fixed_point m_pos, bn::camera_ptr m_camera, bn::affine_bg_ptr m_map);
+            void t_update_position(bn::affine_bg_ptr map, sbb::Level level);
     };
 }
 
