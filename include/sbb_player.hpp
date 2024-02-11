b7a54aa0f9fdf5e2047378223e6146870e12df7d
Dane Petersen <thegreatsunra@gmail.com>
2024-02-11 08:43:38 -0600

    Implement collisions and camera, sort functions

--- include/sbb_player.hpp
+++ include/sbb_player.hpp
@@ -8,6 +8,7 @@
 #include "bn_span.h"
 #include "bn_sprite_ptr.h"
 
+#include "sbb_hitbox.hpp"
 #include "sbb_level.hpp"
 
 namespace sbb
@@ -15,21 +16,35 @@ namespace sbb
     class Player
     {
         private:
-            bn::sprite_ptr m_sprite;
+            bn::optional<bn::camera_ptr> m_camera;
             bn::fixed m_dx;
             bn::fixed m_dy;
-            bn::fixed_point m_pos;
-            bn::optional<bn::camera_ptr> m_camera;
-            bn::optional<bn::span<const bn::affine_bg_map_cell>> m_map_cells;
+            //used for state management
+            bool m_is_jumping = false;
+            bool m_is_falling = false;
+            bool m_is_running = false;
+            bool m_is_grounded = false;
+            bool m_is_sliding = false;
+            bool m_is_already_running = false;
             bn::optional<bn::affine_bg_ptr> m_map;
+            bn::optional<bn::span<const bn::affine_bg_map_cell>> m_map_cells;
+            bn::fixed_point m_pos;
+            bn::sprite_ptr m_sprite;
             void m_update_camera(int lerp);
+            sbb::Hitbox m_hitbox_fall = Hitbox(0, 8, 8, 0);
+            sbb::Hitbox m_hitbox_left = Hitbox(-4, 0, 4, 8);
+            sbb::Hitbox m_hitbox_right = Hitbox(4, 0, 6, 8);
+            sbb::Hitbox m_hitbox_jump = Hitbox(0, 4, 6, 4);
+
         public:
             Player(bn::sprite_ptr m_sprite);
-    void t_move_right();
-    void t_move_left();
-
+            void t_collide_with_objects(bn::affine_bg_ptr map, sbb::Level level);
             void t_delete_data();
             void t_hide();
+            bool t_is_right();
+            void t_move_left();
+            void t_move_right();
+            [[nodiscard]] bn::fixed_point t_pos();
             void t_reset();
             void t_spawn(bn::fixed_point m_pos, bn::camera_ptr m_camera, bn::affine_bg_ptr m_map);
             void t_update_position(bn::affine_bg_ptr map, sbb::Level level);
