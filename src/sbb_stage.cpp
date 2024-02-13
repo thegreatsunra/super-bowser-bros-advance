e3a596500f9675cf038e34d1b8c71808815900eb
Dane Petersen <thegreatsunra@gmail.com>
2024-02-13 08:08:31 -0600

    Move background image with stage image

--- src/sbb_stage.cpp
+++ src/sbb_stage.cpp
@@ -23,10 +23,12 @@ namespace sbb
         bn::camera_ptr camera = bn::camera_ptr::create(spawn_location.x(), spawn_location.y());
         bn::regular_bg_ptr map_bg = bn::regular_bg_items::background.create_bg(512, 512);
         bn::affine_bg_ptr map = bn::affine_bg_items::stage.create_bg(512, 512);
+        map_bg.set_priority(2);
         map.set_priority(1);
         // we'll need this eventually for player:set_position()
         sbb::Level level = sbb::Level(map);
         map.set_camera(camera);
+        map_bg.set_camera(camera);
         m_player->t_spawn(spawn_location, camera, map);
 
         while (true) {
