8cf39cae7e40294edc8fc52e8ad6d50c3e934872
Dane Petersen <thegreatsunra@gmail.com>
2024-02-12 22:17:55 -0600

    Render the background behind everything else even if it all looks weird

--- src/sbb_path.cpp
+++ src/sbb_path.cpp
@@ -2,11 +2,12 @@
 #include "bn_affine_bg_map_cell.h"
 #include "bn_affine_bg_map_ptr.h"
 #include "bn_affine_bg_ptr.h"
+#include "bn_regular_bg_ptr.h"
 #include "bn_camera_ptr.h"
 #include "bn_core.h"
 
-#include "bn_affine_bg_items_path.h"
-// #include "bn_sprite_items_bowser_sprite.h"
+#include "bn_affine_bg_items_ground.h"
+#include "bn_regular_bg_items_background.h"
 
 #include "sbb_path.hpp"
 #include "sbb_player.hpp"
@@ -20,7 +21,8 @@ namespace sbb
     Scene Path::execute(bn::fixed_point spawn_location)
     {
         bn::camera_ptr camera = bn::camera_ptr::create(spawn_location.x(), spawn_location.y());
-        bn::affine_bg_ptr map = bn::affine_bg_items::path.create_bg(512, 512);
+        bn::regular_bg_ptr map_bg = bn::regular_bg_items::background.create_bg(512, 512);
+        bn::affine_bg_ptr map = bn::affine_bg_items::ground.create_bg(512, 512);
         map.set_priority(1);
         // we'll need this eventually for player:set_position()
         sbb::Level level = sbb::Level(map);
