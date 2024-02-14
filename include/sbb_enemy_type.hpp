576cd2f60c0a62d27ec2324038f4f60204b9ef47
Dane Petersen <thegreatsunra@gmail.com>
2024-02-13 19:48:12 -0600

    Tweak whitespace

--- include/sbb_enemy_type.hpp
+++ include/sbb_enemy_type.hpp
@@ -1,10 +1,11 @@
 #ifndef SBB_ENEMY_TYPE_H
 #define SBB_ENEMY_TYPE_H
 
-namespace sbb {
-enum class ENEMY_TYPE {
-    MARIO
-};
+namespace sbb
+{
+    enum class ENEMY_TYPE {
+        MARIO
+    };
 }
 
 #endif
