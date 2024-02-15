fde226a8fd6794106780bc343b0a8585d12cf83a
Dane Petersen <thegreatsunra@gmail.com>
2024-02-14 21:11:48 -0600

    Implement check_collisions function

--- include/sbb_collision.hpp
+++ include/sbb_collision.hpp
@@ -1,13 +1,15 @@
 #ifndef SBB_COLLISION_H
 #define SBB_COLLISION_H
 
+#include "bn_fixed_point.h"
+
+#include "sbb_hitbox.hpp"
+
 namespace sbb
 {
-    class Collision
-    {
-        public:
-            Collision();
-    };
+    bool check_collisions(Hitbox boxA, Hitbox boxB);
+
+    bool check_collisions(Hitbox boxA, bn::fixed x, bn::fixed y, bn::fixed w, bn::fixed h);
 }
 
 #endif
